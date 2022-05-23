dff269df2759e1c2c79eabe1c6d64e703145d729
madhubala2022 <103095880+madhubala2022@users.noreply.github.com>
2022-05-23 20:56:54 +0530

    Update image.groovy

--- src/image.groovy
+++ src/image.groovy
@@ -14,6 +14,16 @@ String buildImage() {
 }
 
 String chartUpdation() {
+  environment {
+      IMAGE_REPO = "madhubala1997/rsvp"
+      REPO = "rsvp"
+      HELM_REPO = "rsvpapp-helm-cicd"
+      GIT_CREDS = credentials('github-token')
+      HELM_GIT_REPO_URL = "github.com/madhubala2022/rsvpapp-helm-cicd.git"
+      GIT_REPO_EMAIL = "madhubala.ravichandran@mindtree.com"
+      GIT_REPO_BRANCH = "master"
+      WEBHOOK = "https://mindtreeonline.webhook.office.com/webhookb2/329f59da-c0a6-4edb-b0a1-cbd712509488@85c997b9-f494-46b3-a11d-772983cf6f11/JenkinsCI/22f5c0f87ca8436a82684dc70b5d509b/961ab056-0929-4c45-9d67-de9017c84fb0"
+  }
   container('tools') {
             sh "git clone https://${env.HELM_GIT_REPO_URL}"
             sh "git config --global user.email ${env.GIT_REPO_EMAIL}"
