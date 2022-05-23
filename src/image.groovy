2b37b995702d9817b063dc59b6d379547175f25f
madhubala2022 <103095880+madhubala2022@users.noreply.github.com>
2022-05-23 22:23:48 +0530

    Update image.groovy

--- src/image.groovy
+++ src/image.groovy
@@ -9,7 +9,6 @@ env.GIT_REPO_EMAIL = "madhubala.ravichandran@mindtree.com"
 env.GIT_REPO_BRANCH = "master"
 env.WEBHOOK = "https://mindtreeonline.webhook.office.com/webhookb2/329f59da-c0a6-4edb-b0a1-cbd712509488@85c997b9-f494-46b3-a11d-772983cf6f11/JenkinsCI/22f5c0f87ca8436a82684dc70b5d509b/961ab056-0929-4c45-9d67-de9017c84fb0"
 
-
 String buildImage() {
   container('kaniko') {
           script {
