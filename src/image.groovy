a94108ed09e57278cefc4d2cf91ede8b61d48c88
madhubala2022 <103095880+madhubala2022@users.noreply.github.com>
2022-05-24 17:21:11 +0530

    Update image.groovy

--- src/image.groovy
+++ src/image.groovy
@@ -1,12 +1,12 @@
 print("Loaded class Image.groovy")
 
-env.IMAGE_REPO = "madhubala1997/rsvp"
-env.REPO = "rsvp"
+env.IMAGE_REPO = "madhubala1997/sample"
+env.REPO = "sample"
 env.HELM_REPO = "rsvpapp-helm-cicd"
 env.GIT_CREDS = credentials('github-token')
 env.HELM_GIT_REPO_URL = "github.com/madhubala2022/rsvpapp-helm-cicd.git"
 env.GIT_REPO_EMAIL = 'madhubala.ravichandran@mindtree.com'
-env.GIT_REPO_BRANCH = "master"
+env.GIT_REPO_BRANCH = "dev"
 env.WEBHOOK = "https://mindtreeonline.webhook.office.com/webhookb2/329f59da-c0a6-4edb-b0a1-cbd712509488@85c997b9-f494-46b3-a11d-772983cf6f11/JenkinsCI/22f5c0f87ca8436a82684dc70b5d509b/961ab056-0929-4c45-9d67-de9017c84fb0"
 
 String buildImage() {
@@ -30,7 +30,7 @@ String chartUpdation() {
             sh "wget https://github.com/mikefarah/yq/releases/download/v4.9.6/yq_linux_amd64.tar.gz"
             sh "tar xvf yq_linux_amd64.tar.gz"
             sh "mv yq_linux_amd64 /usr/bin/yq"
-            sh "git checkout -b master"
+            sh "git checkout -b ${env.GIT_REPO_BRANCH}"
           dir("rsvpapp-helm-cicd") {
               sh "git checkout ${env.GIT_REPO_BRANCH}"
             //install done
