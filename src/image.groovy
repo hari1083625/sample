2c67ac0e814a8317468a71e5017dd30ef09030c5
madhubala2022 <103095880+madhubala2022@users.noreply.github.com>
2022-05-24 13:39:06 +0530

    Update image.groovy

--- src/image.groovy
+++ src/image.groovy
@@ -3,10 +3,6 @@ print("Loaded class Image.groovy")
 env.IMAGE_REPO = "madhubala1997/rsvp"
 env.REPO = "rsvp"
 env.HELM_REPO = "rsvpapp-helm-cicd"
-env.GIT_CREDS = credentials('github-token')
-env.HELM_GIT_REPO_URL = "github.com/madhubala2022/rsvpapp-helm-cicd.git"
-env.GIT_REPO_EMAIL = 'madhubala.ravichandran@mindtree.com'
-env.GIT_REPO_BRANCH = "master"
 env.WEBHOOK = "https://mindtreeonline.webhook.office.com/webhookb2/329f59da-c0a6-4edb-b0a1-cbd712509488@85c997b9-f494-46b3-a11d-772983cf6f11/JenkinsCI/22f5c0f87ca8436a82684dc70b5d509b/961ab056-0929-4c45-9d67-de9017c84fb0"
 
 String buildImage() {
@@ -44,10 +40,10 @@ String chartUpdation() {
               pwd
               git add values.yaml
               git commit -m 'Triggered Build'
-              git push "https://${env.GIT_CREDS_USR}:${env.GIT_CREDS_PSW}@github.com/${env.GIT_CREDS_USR}/rsvpapp-helm-cicd.git"
+              git push https://$GIT_CREDS_USR:$GIT_CREDS_PSW@github.com/$GIT_CREDS_USR/rsvpapp-helm-cicd.git
             '''
           }
-            }
+        }
 }
 
 return this
